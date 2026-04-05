1f6bfd84479254fb5103c2acd9d95075a810a21b
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2026-04-05 16:30:58 +0600

    Add missing import for Service annotation in ProductService

--- e-com-backend/ecom-proj/src/main/java/com/rahul/ecom_proj/service/ProductService.java
+++ e-com-backend/ecom-proj/src/main/java/com/rahul/ecom_proj/service/ProductService.java
@@ -3,10 +3,12 @@ package com.rahul.ecom_proj.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.rahul.ecom_proj.model.Product;
 import com.rahul.ecom_proj.repo.ProductRepo;
 
+@Service // Spring annotation to indicate that this class is a service component
 public class ProductService {
 
     @Autowired
