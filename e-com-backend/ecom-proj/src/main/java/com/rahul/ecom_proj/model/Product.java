8cc964272b21c362a27055cbd69c79f36d962f44
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2026-04-05 12:53:42 +0600

    Added Product entity with JPA annotations and Lombok support

---
+++ e-com-backend/ecom-proj/src/main/java/com/rahul/ecom_proj/model/Product.java
@@ -0,0 +1,29 @@
+package com.rahul.ecom_proj.model;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity // JPA annotation to mark this class as a database entity
+@Data   // Lombok annotations to generate boilerplate code like getters, setters, constructors, etc.
+@AllArgsConstructor // Generates a constructor with all fields as parameters
+@NoArgsConstructor // Generates a no-argument constructor
+public class Product {
+
+    @Id // JPA annotation to mark this field as the primary key 
+    private int id;
+    private String name;
+    private String desc;
+    private String brand;
+    private BigDecimal price;
+    private String category;
+    private Date releaseDate;
+    private boolean available;
+    private int quantity;
+}
