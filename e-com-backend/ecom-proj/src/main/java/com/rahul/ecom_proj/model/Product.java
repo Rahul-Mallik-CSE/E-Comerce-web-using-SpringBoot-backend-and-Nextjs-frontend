b6b452b06166de0326d782d30e316c514156f230
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2026-04-08 14:59:26 +0600

    modify Product entity: change ID type from int to Integer and remove unused annotations

--- e-com-backend/ecom-proj/src/main/java/com/rahul/ecom_proj/model/Product.java
+++ e-com-backend/ecom-proj/src/main/java/com/rahul/ecom_proj/model/Product.java
@@ -5,24 +5,22 @@ import java.util.Date;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
-import jakarta.annotation.Generated;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Lob;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity // JPA annotation to mark this class as a database entity
 @Data   // Lombok annotations to generate boilerplate code like getters, setters, constructors, etc.
-@AllArgsConstructor // Generates a constructor with all fields as parameters
 @NoArgsConstructor // Generates a no-argument constructor
 public class Product {
 
     @Id // JPA annotation to mark this field as the primary key 
     @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY) // Auto-generates the ID value
-    private int id;
+    private Integer id;
     private String name;
     private String desc;
     private String brand;
