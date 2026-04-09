b76243d942b7ff3823810a329662abca9fcf6c14
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2026-04-09 12:12:42 +0600

    Add deleteProduct method in ProductService and corresponding endpoint in ProductController for product deletion

--- e-com-backend/ecom-proj/src/main/java/com/rahul/ecom_proj/controller/ProductController.java
+++ e-com-backend/ecom-proj/src/main/java/com/rahul/ecom_proj/controller/ProductController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -82,11 +83,25 @@ public class ProductController {
          catch(IOException e){
           return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
          }
+         
          if(product1 != null){
           return new ResponseEntity<>("Product updated successfully", HttpStatus.OK);
          }
          else{
           return new ResponseEntity<>("Product not found", HttpStatus.NOT_FOUND);
          }
+    }
+
+    //delete product
+    @DeleteMapping("/product/{id}")
+    public ResponseEntity<String> deleteProduct(@PathVariable int id){
+        Product product = service.getProductById(id);
+        if(product != null){
+            service.deleteProduct(id);
+            return new ResponseEntity<>("Product deleted successfully", HttpStatus.OK);
+        }
+        else{
+            return new ResponseEntity<>("Product not found", HttpStatus.NOT_FOUND);
+        }
     }
 } 
