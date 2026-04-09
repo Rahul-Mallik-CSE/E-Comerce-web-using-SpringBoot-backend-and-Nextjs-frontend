6fa89fdb6df77a03853fe5b284c7e3b057bc9474
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2026-04-09 12:23:16 +0600

    updated updateProduct endpoint to use PUT method and adjust parameter annotations

--- e-com-backend/ecom-proj/src/main/java/com/rahul/ecom_proj/controller/ProductController.java
+++ e-com-backend/ecom-proj/src/main/java/com/rahul/ecom_proj/controller/ProductController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
@@ -73,8 +74,8 @@ public class ProductController {
     }
 
     //update product
-    @PostMapping("/product/{id}")
-    public ResponseEntity<String> updateProduct(@PathVariable int id , @RequestBody Product product ,@RequestPart MultipartFile imageFile   ){
+    @PutMapping("/product/{id}")
+    public ResponseEntity<String> updateProduct(@PathVariable int id , @RequestPart Product product ,@RequestPart MultipartFile imageFile   ){
         
         Product product1 = null;
         try{
